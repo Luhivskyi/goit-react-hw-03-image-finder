a92c42bc5583c65d1c7a8d6169c0e069236e07df
Luhivskyi <luhivskyi.d@ukr.net>
2021-05-30 10:00:38 +0300

    start hwR3

---
+++ src/Components/ImageGallery.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+import '../styles.css';
+
+function ImageGallery({ images, openModal }) {
+  return (
+    <ul className="ImageGallery">
+      {images.map(({ id, webformatURL, largeImageURL }) => (
+        <li
+          key={id}
+          className="ImageGalleryItem"
+          onClick={() => openModal(largeImageURL)}
+        >
+          <img src={webformatURL} alt="" className="ImageGalleryItem-image" />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ImageGallery;
---
+++ src/Components/LoadMoreButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../styles.css';
+
+class LoadMoreButton extends Component {
+  componentDidMount() {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  }
+
+  render() {
+    return (
+      <button
+        type="button"
+        className="Button"
+        onClick={() => this.props.loadMore()}
+      >
+        Load more
+      </button>
+    );
+  }
+}
+
+export default LoadMoreButton;
